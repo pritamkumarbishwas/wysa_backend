096d2faae4f736dd6637ae1dd484c1b9b4cb8db4
pritamkumarbishwas <pritamkumarbishwas85@gmail.com>
2024-09-15 17:31:20 +0530

    chage in response

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -20,7 +20,7 @@ export const verifyJWT = asyncHandler(async(req, _, next) => {
             throw new ApiError(401, "Invalid Access Token")
         }
     
-        req.user = user;
+        req.user = user;  
         next()
     } catch (error) {
         throw new ApiError(401, error?.message || "Invalid access token")
